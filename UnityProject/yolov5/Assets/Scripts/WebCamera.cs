1f58a3f16e51a8734dba6701a4c5858e6a82a537
xmy <xmycissy@outlook.com>
2022-06-14 22:57:52 +0800

    first commit

---
+++ UnityProject/yolov5/Assets/Scripts/WebCamera.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+class WebCamera : MonoBehaviour
+{
+
+    bool isCamAvailable;
+    WebCamTexture cameraTexture;
+    Texture defaultBackground;
+
+    public RawImage background;
+    public AspectRatioFitter fit;
+
+    private void Start()
+    {
+        defaultBackground = background.texture;
+
+        WebCamDevice[] devices = WebCamTexture.devices;
+
+        if(devices.Length == 0)
+        {
+            Debug.Log("No camera detected");
+            isCamAvailable = false;
+            return;
+        }
+
+        foreach (WebCamDevice device in devices)
+        {
+            if (!device.isFrontFacing)
+            {
+                cameraTexture = new WebCamTexture(device.name, Screen.width, Screen.height); ;
+            }
+        }
+
+        if (cameraTexture == null)
+        {
+            Debug.Log("No camera detected");
+            return;
+        }
+
+        cameraTexture.Play();
+        background.texture = cameraTexture;
+
+        isCamAvailable = true;
+    }
+
+    private void Update()
+    {
+        if (!isCamAvailable)
+        {
+            return;
+        }
+
+        float ratio= (float)cameraTexture .width / (float)cameraTexture.height;
+
+        fit.aspectRatio = ratio;
+
+        float scaleY = cameraTexture.videoVerticallyMirrored ? -1f : 1f;
+
+        background.rectTransform.localScale = new Vector3(1f,scaleY,1f);
+
+
+        int orient = -cameraTexture.videoRotationAngle;
+        background.rectTransform.localEulerAngles = new Vector3(0,0,orient);
+    }
+}
+
